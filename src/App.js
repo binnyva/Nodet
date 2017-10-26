e06e401e139982c41f386fafbf6a8bcc457de097
Binny V A <binnyva@gmail.com>
2017-10-26 19:43:51 +0530

    Hiding now works cleaner. Added Bootstap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-theme.min.css';
 import data from './data.json';
 
 class Node extends Component {
@@ -10,10 +12,14 @@ class Node extends Component {
 		this.state = {
 			value: props.node.title,
 			childCount: child_count,
-			plus: ''
+			status: 'open',
+			plus: '',
+			plusIcon: ''
 		}
 		
+		// The limitations of the ES6 syntax.
 	    this.handleChange = this.handleChange.bind(this);
+	    this.toggle = this.toggle.bind(this);
 	}
 	componentDidMount() {
 		this.setText();
@@ -25,24 +31,32 @@ class Node extends Component {
 	}
 
 	toggle() {
-		this.state.status = (this.state.status === 'open') ? 'close' : 'open'; // Intentionally not using setState. Or the next line won't work as expected.
+		if(this.state.status === 'open'){
+			this.state.status = 'close';
+		} else {
+			this.state.status = 'open'; // Intentionally not using setState. Or the next line won't work as expected.
+		}
 		this.setText();
 	}
 
 	setText() {
-		console.log(this.state)
 		if(this.state.childCount > 0) {
-			if(this.state.status === 'open') this.setState({plus:'-'});
-			else if(this.state.status === 'close') this.setState({plus:'+'});
+			if(this.state.status === 'open') {
+				this.setState({plus:'-'});
+				this.setState({plusIcon:'glyphicon-minus'});
+			} else if(this.state.status === 'close') {
+				this.setState({plus:'+'});
+				this.setState({plusIcon:'glyphicon-plus'});
+			}
 		}
 	}
 
 	render() {
 		return (
-			<li>
-				<span className="open-status" onClick={this.toggle}>{this.state.plus}</span>
-				<input type="text" value={this.state.value} onChange={this.handleChange} />
-				<TreeChildren nodes={this.props.node.children} />
+			<li className="node">
+				<span className={'open-status glyphicon ' + this.state.plusIcon} onClick={this.toggle}></span>
+				<input type="text" className="node-text" value={this.state.value} onChange={this.handleChange} />
+				<TreeChildren nodes={this.props.node.children} status={this.state.status} />
 			</li>
 		);
 	}
@@ -53,8 +67,11 @@ class TreeChildren extends Component {
 		var items;
 		if(this.props.nodes) items = this.props.nodes.map((node) => <Node key={node.id} node={node} />);
 
+		var className = "show";
+		if(this.props.status === "close") className = "hide";
+
 		if(items) return (
-			<ul>
+			<ul className={className}>
 				{items}
 			</ul>
 		);
